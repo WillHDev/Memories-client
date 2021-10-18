400cdcf8d9bd36e63032b0e8c42aee84b623f63f
Will Holcombe <will.v.holcombe@gmail.com>
2021-10-17 20:33:47 -0400

    css working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import SignUp from './components/Auth/SignUp'
 //import moduleName from 'react-router-dom';
 
 export default function App() {
     return (
         <div>
             <h1>App</h1>
+                 <SignUp />
         </div>
     )
 }
--- src/components/Auth/SignUp.js
+++ src/components/Auth/SignUp.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Form, Button, Card } from 'react-bootstrap'
 
 
 export default function SignUp() {
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+
     return (
         <>
             <Card>
                 <Card.Body>
-                    <h2>Sign Up</h2>
+                    <h2 className="text-center mb-4">Sign Up</h2>
                     <Form>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" />
+                            <Form.Control type="email" ref={emailRef} />
                         </Form.Group>
+                        <Form.Group id="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="email" ref={passwordRef} />
+                        </Form.Group>
+                        <Form.Group id="password-confirm">
+                            <Form.Label>Password Confirmation</Form.Label>
+                            <Form.Control type="email" ref={passwordConfirmRef} />
+                        </Form.Group>
+                        <Button className="w-100" type="submit">Sign Up</Button>
                     </Form>
                 </Card.Body>
             </Card>
