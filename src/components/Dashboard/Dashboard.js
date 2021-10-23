ababd5b704c49c04d89694869f8c1e2e2b61b4e3
Will Holcombe <will.v.holcombe@gmail.com>
2021-10-23 12:30:12 -0400

    added logout button

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
+            <Link to="/login">Logout</Link> 
         </div>
     )
 }
